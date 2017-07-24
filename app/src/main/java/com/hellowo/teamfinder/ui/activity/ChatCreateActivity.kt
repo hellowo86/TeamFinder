04e78a8cf8d0df3f1ee60f01532e52bb8d2651b3
kyeonghun.seo@day2life.com <kyeonghun.seo@day2life.com>
2017-07-24 18:22:11 +0900

    commit in company

--- app/src/main/java/com/hellowo/teamfinder/ui/activity/ChatCreateActivity.kt
+++ app/src/main/java/com/hellowo/teamfinder/ui/activity/ChatCreateActivity.kt
@@ -1,20 +1,27 @@
 package com.hellowo.teamfinder.ui.activity
 
 import android.arch.lifecycle.LifecycleActivity
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.animation.AnimationUtils
+import android.view.inputmethod.InputMethodManager
 import com.hellowo.teamfinder.R
-import com.hellowo.teamfinder.viewmodel.MainViewModel
+import com.hellowo.teamfinder.viewmodel.ChatCreateViewModel
 import kotlinx.android.synthetic.main.activity_chat_create.*
+import com.hellowo.teamfinder.viewmodel.ChatCreateViewModel.CurrentProgress
+import com.hellowo.teamfinder.viewmodel.ChatCreateViewModel.CurrentProgress.*
 
 class ChatCreateActivity : LifecycleActivity() {
-    lateinit var viewModel: MainViewModel
+    lateinit var viewModel: ChatCreateViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_create)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(ChatCreateViewModel::class.java)
         initLayout()
         initObserve()
     }
@@ -22,10 +29,57 @@ class ChatCreateActivity : LifecycleActivity() {
     fun initLayout() {
         viewFlipper.inAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_in_right)
         viewFlipper.outAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_out_left)
-        confirmBtn.setOnClickListener { viewFlipper.showNext() }
+        confirmBtn.setOnClickListener { viewModel.goNextProgress() }
+        backBtn.setOnClickListener{ onBackPressed() }
+        titleInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                viewModel.setTitle(s)
+            }
+        })
+        contentsInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                viewModel.setContents(s)
+            }
+        })
+
+        titleInput.postDelayed({
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(titleInput, InputMethodManager.SHOW_IMPLICIT)
+        }, 250)
     }
 
     fun initObserve() {
+        viewModel.currentProgress.observe(this, Observer { updateProgressUI(it) })
+        viewModel.checkCanGoNextProgress.observe(this, Observer { updateConfirmBtn(it) })
+    }
 
+    private fun updateConfirmBtn(isCanGoNextProgress: Boolean?) {
+        if(isCanGoNextProgress as Boolean) {
+            confirmBtn.setTextColor(getColor(R.color.colorPrimary))
+            confirmBtn.setBackgroundResource(R.drawable.ripple_button)
+        }else {
+            confirmBtn.setTextColor(getColor(R.color.disableText))
+            confirmBtn.setBackgroundResource(R.color.transparent)
+        }
+    }
+
+    private fun updateProgressUI(progress: CurrentProgress?) {
+        viewFlipper.displayedChild = progress?.ordinal ?: 0
+        if(viewModel.currentProgress.value == Options) {
+            confirmBtn.setText(R.string.confirm)
+        }else {
+            confirmBtn.setText(R.string.next)
+        }
+    }
+
+    override fun onBackPressed() {
+        when(viewModel.currentProgress.value) {
+            Title -> finish()
+            else -> viewModel.goPreviousProgress()
+        }
     }
 }
--- app/src/main/java/com/hellowo/teamfinder/viewmodel/ChatCreateViewModel.kt
+++ app/src/main/java/com/hellowo/teamfinder/viewmodel/ChatCreateViewModel.kt
@@ -1,9 +1,55 @@
 package com.hellowo.teamfinder.viewmodel
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.text.Editable
+import com.hellowo.teamfinder.model.Chat
+import com.hellowo.teamfinder.viewmodel.ChatCreateViewModel.CurrentProgress.*
 
 class ChatCreateViewModel : ViewModel() {
+    enum class CurrentProgress {Title, Contents, Options}
+    val currentProgress = MutableLiveData<CurrentProgress>()
+    val checkCanGoNextProgress = MutableLiveData<Boolean>()
+    val chat = Chat()
 
     init {
+        currentProgress.value = CurrentProgress.Title
+        checkCanGoNextProgress.value = false
+    }
+
+    fun goNextProgress() {
+        if(checkCanGoNextProgress.value == true) {
+            when(currentProgress.value) {
+                Options -> confirm()
+                else -> currentProgress.value = CurrentProgress.values()[currentProgress.value?.ordinal?.plus(1) as Int]
+            }
+        }
+    }
+
+    fun goPreviousProgress() {
+        currentProgress.value = CurrentProgress.values()[currentProgress.value?.ordinal?.minus(1) as Int]
+    }
+
+    private fun confirm() {
+
+    }
+
+    fun  setTitle(s: Editable) {
+        chat.title = s.toString()
+        checkCanGoNextProgress()
+    }
+
+    fun  setContents(s: Editable) {
+        chat.description = s.toString()
+        checkCanGoNextProgress()
+    }
+
+    fun checkCanGoNextProgress() {
+        when(currentProgress.value) {
+            Title -> if(!chat.title.isNullOrBlank()) checkCanGoNextProgress.value = true
+            Contents -> if(!chat.description.isNullOrBlank()) checkCanGoNextProgress.value = true
+            Options -> checkCanGoNextProgress.value = true
+            else -> checkCanGoNextProgress.value = false
+        }
     }
 }
